8a293bb95e7feff348853d5723f75ba836b64d94
Fabby-B <babuhfabby@gmail.com>
2019-02-25 16:53:27 +0300

    first commit

---
+++ src/xyz/fileThree.java
@@ -0,0 +1,16 @@
+package xyz;
+
+public class fileThree {
+    public static void main(String[] args) {
+        String[]wanafunzi;
+        String output;
+        wanafunzi= new String[5];
+        wanafunzi[0]="Anthony";
+        wanafunzi[1]="Ian";
+        wanafunzi[2]="Peter";
+        wanafunzi[3]="Brayo";
+        wanafunzi[4]="Wasike";
+        output=wanafunzi[0]+" "+wanafunzi[1]+" "+wanafunzi[2]+" "+wanafunzi[3]+" "+wanafunzi[4];
+        System.out.println(output);
+    }
+}
